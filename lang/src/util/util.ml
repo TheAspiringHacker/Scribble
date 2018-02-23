d578a895b2f7f3ff48083238b6ac12a3eec1a37d
TheAspiringHacker <TheAspiringHacker@protonmail.com>
2018-02-23 10:17:56 -0500

    Removed >> and bind from monad intf, added state monad, added func-to-op funcs

--- lang/src/util/util.ml
+++ lang/src/util/util.ml
@@ -1,20 +1,20 @@
+(** https://github.com/ocaml/ocaml/pull/1549 See infix operator workaround *)
+let ($<) x f = f x
+let (>$) f x = f x
+
 module type MONAD = sig
   type 'a m
-  val bind : 'a m -> ('a -> 'b m) -> 'b m
-  val return : 'a -> 'a m
   val (>>=) : 'a m -> ('a -> 'b m) -> 'b m
-  val (>>) : 'a m -> 'b m -> 'b m
+  val return : 'a -> 'a m
 end
 
 module Option : MONAD with type 'a m := 'a option = struct
   type 'a m = 'a option
-  let bind m f =
+  let (>>=) m f =
     match m with
     | Some x -> f x
     | None -> None
   let return x = Some x
-  let (>>=) = bind
-  let (>>) m0 m1 = m0 >>= fun _ -> m1
 end
 
 type ('a, 'b) result = Err of 'a | Ok of 'b
@@ -23,11 +23,19 @@ module Result (X : sig
   type t
 end) : MONAD with type 'a m := (X.t, 'a) result = struct
   type 'a m = (X.t, 'a) result
-  let bind (m : 'a m) (f : 'a -> 'b m) =
+  let (>>=) (m : 'a m) (f : 'a -> 'b m) =
     match m with
     | Ok x -> f x
     | Err err -> Err err
   let return x = Ok x
-  let (>>=) = bind
-  let (>>) m0 m1 = m0 >>= fun _ -> m1
+end
+
+module State (X : sig
+  type t
+end) : MONAD with type 'a m := X.t -> ('a * X.t) = struct
+  type 'a m = X.t -> ('a * X.t)
+  let (>>=) m f st =
+    let (a, st) = m st in
+    f a st
+  let return a st = (a, st)
 end
