c9fb6509e0479a204367237a8594227f799fe176
TheAspiringHacker <TheAspiringHacker@protonmail.com>
2018-02-23 18:21:21 -0500

    Replace := with = in modules (Thanks https://stackoverflow.com/a/48954754/8887578)

--- lang/src/util/util.ml
+++ lang/src/util/util.ml
@@ -8,7 +8,22 @@ module type MONAD = sig
   val return : 'a -> 'a m
 end
 
-module Option : MONAD with type 'a m := 'a option = struct
+module Identity : MONAD with type 'a m = 'a = struct
+  type 'a m = 'a
+  let (>>=) m f = f m
+  let return x = x
+end
+
+module OptionT (M : MONAD) : MONAD with type 'a m = ('a option) M.m = struct
+  type 'a m = ('a option) M.m
+  let (>>=) m f = M.(>>=) m (fun option ->
+    match option with
+    | Some x -> f x
+    | None -> M.return None)
+  let return x = M.return @@ Some x
+end
+
+module Option : MONAD with type 'a m = 'a option = struct
   type 'a m = 'a option
   let (>>=) m f =
     match m with
@@ -19,20 +34,22 @@ end
 
 type ('a, 'b) result = Err of 'a | Ok of 'b
 
-module Result (X : sig
+module ResultT (M : MONAD) (X : sig
   type t
-end) : MONAD with type 'a m := (X.t, 'a) result = struct
-  type 'a m = (X.t, 'a) result
-  let (>>=) (m : 'a m) (f : 'a -> 'b m) =
-    match m with
+end) : MONAD with type 'a m = ((X.t, 'a) result) M.m = struct
+  type 'a m = ((X.t, 'a) result) M.m
+  let (>>=) m f = m $<M.(>>=)>$ fun result ->
+    match result with
     | Ok x -> f x
-    | Err err -> Err err
-  let return x = Ok x
+    | Err err -> M.return @@ Err err
+  let return x = M.return @@ Ok x
 end
+
+module Result = ResultT(Identity)
 
 module State (X : sig
   type t
-end) : MONAD with type 'a m := X.t -> ('a * X.t) = struct
+end) : MONAD with type 'a m = X.t -> ('a * X.t) = struct
   type 'a m = X.t -> ('a * X.t)
   let (>>=) m f st =
     let (a, st) = m st in
